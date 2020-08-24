3b04be8ddf8241139a9a12e691af8ab40233a467
jakubG78 <jakub.gora@wp.pl>
2020-08-24 21:37:14 +0200

    initial commit

---
+++ demo-books/src/main/java/com/vaadin/demobooks/DemoBooksApplication.java
@@ -0,0 +1,13 @@
+package com.vaadin.demobooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBooksApplication.class, args);
+    }
+
+}
